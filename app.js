1ac3d1bb9321fee8736656928e6b2fde8d6ecec6
tomek-zawadzki <65092372+tomek-zawadzki@users.noreply.github.com>
2022-11-14 23:57:45 +0100

    add div to change name

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const popupInstruction = document.querySelector(".popup-instruction");
 const closePopup = document.querySelector(".btn-close");
 const openPopup = document.querySelector(".show-instruction");
+const btnCloseChangeName = document.querySelector(".btn-close-chn");
+const boxChangeName = document.querySelector(".change-name");
+const inputChangeName = document.querySelector("#new-name");
+const playerOne = document.querySelector(".player-one");
+const playerTwo = document.querySelector(".player-two");
 
 const showPopup = () => {
   popupInstruction.style.display = "flex";
